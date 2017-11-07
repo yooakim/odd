79a2375cdd149e804a6bab934c065b4b901bc600
Joakim Westin <joakim.westin@basefarm.com>
2017-11-07 10:45:54 +0100

    Added AppInsight key

--- ODD.Web/Global.asax.cs
+++ ODD.Web/Global.asax.cs
@@ -13,6 +13,7 @@ namespace ODD.Web
     {
         protected void Application_Start()
         {
+
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
